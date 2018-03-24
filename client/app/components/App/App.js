9caf9e9c4acae160f017851cf3437f66d975626f
Ahmed Saad <a7med.s3d.ibrahim@gmail.com>
2018-03-25 00:08:52 +0200

    started working on the todo feature

--- client/app/components/App/App.js
+++ client/app/components/App/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <Header />
-
                 <div>
                     {this.props.children}
                 </div>
-
-                <Footer />
             </div>
         );
     }
---
+++ client/app/components/Todo/TodoList.js
@@ -0,0 +1,8 @@
+import react, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class TodoList extends Component {
+    render(){
+
+    }
+}
